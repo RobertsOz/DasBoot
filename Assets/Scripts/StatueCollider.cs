9b914a40d42a83098d2b150f0f0ca2524a1a41b4
Cliffjord <cliffordmckinney1997@gmail.com>
2017-11-17 11:04:45 +0000

    Recommit.

---
+++ Assets/Scripts/StatueCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StatueCollider : MonoBehaviour {
+
+	private GameObject DasBootInfo;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == ("Peasant"))
+		{
+			DasBootInfo = GameObject.Find("DasBootInfo");
+			DasBootInfo.GetComponent<DasBootInfo> ().playerLives -= 1;
+			if (DasBootInfo.GetComponent<DasBootInfo> ().playerLives <= 0) 
+			{
+				SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+			}
+		}
+	}
+}
